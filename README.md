3f4e7fc763a2517517b676b91db72d35499f0331
Iñaki Villar <inaki.seri@gmail.com>
2026-08-12 13:16:15 -0700

    Onboard to the OSS Community Develocity Instance (#55)

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # Graph Analytics Plugin
 
+[![Revved up by Develocity](https://img.shields.io/badge/Revved%20up%20by-Develocity-06A0CE?logo=Gradle&labelColor=02303A)](https://community.develocity.cloud/scans?search.rootProjectNames=graph-analytics-plugin)
+
 ## About This Project
 
 This [Gradle](https://gradle.org/) plugin was designed to be used in multi-module
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,8 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.develocity") version("4.0.2")
+    id("com.gradle.develocity") version("4.5.0")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version("2.8.0")
 }
 
 dependencyResolutionManagement {
@@ -19,9 +20,29 @@ dependencyResolutionManagement {
 
 rootProject.name = "graph-analytics-plugin"
 
+val isCI = System.getenv("CI") != null
+
 develocity {
+    server = "https://community.develocity.cloud"
+    projectId = "ebay"
     buildScan {
-        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
-        termsOfUseAgree.set("yes")
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+        }
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(develocity.buildCache) {
+        isEnabled = true
+        // Check access key presence to avoid build cache errors on PR builds when access key is not present
+        val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+        isPush = isCI && accessKey != null
     }
 }
